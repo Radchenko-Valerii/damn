d0d58074204da8607b0186de28905e7219e53501
Valerii Radchenko <valeravivaz@gmail.com>
2021-08-12 17:02:06 +0300

    comit

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,1 +1,18 @@
 'use strict'
+
+// const [button1, button2, button3, button4] = document.querySelectorAll('button');
+// const [div1] = document.querySelectorAll('div')
+
+function listen(){
+  this.parentElement.style.background = this.dataset.attribute;
+}
+
+// button1.addEventListener('click', listen);
+// button2.addEventListener('click', listen);
+// button3.addEventListener('click', listen);
+// button4.addEventListener('click', listen);
+
+
+for(const button of document.querySelectorAll('button')){
+  button.addEventListener('click', listen)
+}
