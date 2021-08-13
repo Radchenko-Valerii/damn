33a2de3878d141629cbddb1c2a5280aae173377b
Valerii <valeravivaz@gmail.com>
2021-08-13 12:13:04 +0300

    add new logic

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,17 +1,8 @@
 'use strict'
 
-// const [button1, button2, button3, button4] = document.querySelectorAll('button');
-// const [div1] = document.querySelectorAll('div')
-
-function listen(){
-  this.parentElement.style.background = this.dataset.attribute;
+function listen(event){
+  event.target.parentElement.style.backgroundColor = event.target.dataset.attribute;
 }
-
-// button1.addEventListener('click', listen);
-// button2.addEventListener('click', listen);
-// button3.addEventListener('click', listen);
-// button4.addEventListener('click', listen);
-
 
 for(const button of document.querySelectorAll('button')){
   button.addEventListener('click', listen)
